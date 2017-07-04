c2d81018fdf45b033b1f599f0dc42317781e5362
Michael J <mmorel@e1z3r1p8.42.us.org>
2017-07-03 20:06:02 -0700

    Modified to print entire string instead

--- ft_printf.c
+++ ft_printf.c
@@ -31,17 +31,18 @@ int		ft_printf(const char *restrict format, ...)
 		if (format[index] == '%')
 		{
 			printf("%c\n", format[index]);
-			if (format[index + 1] != '\0')
-			{
-				// printf("Activated\n");
-				// index++;
-				// len += ft_strlen(format);
-				// index++;
-				len += ft_printf_parse(format, arguments, &index);
-				index++;
-			}
-			else
-				return (0);
+			// if (format[index + 1] != '\0')
+			// {
+			// 	// printf("Activated\n");
+			// 	// index++;
+			// 	// len += ft_strlen(format);
+			// 	// index++;
+			// 	len += ft_printf_parse(format, arguments, &index);
+			// 	index++;
+			// }
+			// else
+			// 	return (0);
+			index++;
 			// printf("len after parse: %d\n", len);
 		}
 		else
